662cdac660be0e6ab6e14b6bbf6f0c54bc9fea36
ksmyshlyaev <smyshlyaevkonst@gmail.com>
2019-11-06 18:14:26 +0400

    added first realisation

--- src/com/company/EmailAssignment.java
+++ src/com/company/EmailAssignment.java
@@ -4,9 +4,11 @@ public class EmailAssignment {
     static void emailInfo(String emails){
         char c1;
         for(int i=0;i<emails.length();i++){
+            int nextSymbolIndex = 0;
             c1=emails.charAt(i);
-            if(c1=='@'){
-                System.out.println(emails);
+            if(c1==';'){
+                System.out.println(emails.substring(nextSymbolIndex, i));
+                nextSymbolIndex = i++;
             }
         }
     }
